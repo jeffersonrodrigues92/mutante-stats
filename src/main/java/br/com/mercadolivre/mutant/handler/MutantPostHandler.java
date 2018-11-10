573706e06fb782d4337329d235c0f45262d999df
Jefferson Rodrigues <jefferson.rodrigues@MacBook-Pro-de-Jefferson-2.local>
2018-11-10 01:31:44 -0200

    Criando integração API Gateway, Lambda e DynamoDB

--- src/main/java/br/com/mercadolivre/mutant/handler/MutantPostHandler.java
+++ src/main/java/br/com/mercadolivre/mutant/handler/MutantPostHandler.java
@@ -12,17 +12,19 @@ import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+
 
 import br.com.mercadolivre.mutant.request.MutantRequest;
-import br.com.mercadolivre.mutant.response.MutantResponse;;
+import br.com.mercadolivre.mutant.response.MutantResponse;
+import com.amazonaws.services.lambda.runtime.RequestHandler;;
 
-public class MutantPostHandler implements RequestHandler {
+public class MutantPostHandler implements RequestHandler<Map<String,List<String>>, MutantResponse> {
 
 	private DynamoDB dynamoDb;
 	private Regions REGION = Regions.US_EAST_1;
 
-	public MutantResponse handleRequest(Object mutant, Context context) {
+
+	public MutantResponse handleRequest(Map<String, List<String>> mutant, Context context) {
 
 		initDynamoDbClient();
 		MutantRequest mutantRequest = new MutantRequest();
@@ -33,7 +35,7 @@ public class MutantPostHandler implements RequestHandler {
 		mutantRequest.setId(new Date().getTime());
 		mutantRequest.setMutants(3);
 		mutantRequest.setHumans(10);
-		
+
 		MutantResponse response = new MutantResponse();
 
 		response.setMessage("SUCCESS");
@@ -43,7 +45,7 @@ public class MutantPostHandler implements RequestHandler {
 	}
 
 	private PutItemOutcome persistData(MutantRequest personRequest) throws ConditionalCheckFailedException {
-		return this.dynamoDb.getTable("mutant-mercado-livre").putItem(new PutItemSpec()
+		return this.dynamoDb.getTable("mutante-mercado-livre").putItem(new PutItemSpec()
 				.withItem(new Item().with("id", new Date().getTime()).with("mutants", 10).with("humans", 5)));
 	}
 
